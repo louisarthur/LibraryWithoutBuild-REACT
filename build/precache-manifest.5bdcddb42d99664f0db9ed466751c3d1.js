98f7ff59078a0c3d53691eaef1d52d5d8e1478b5
Louis Arthur <louisartur.53@gmail.com>
2019-11-12 20:17:09 -0300

    adding some features and without build

---
+++ build/precache-manifest.5bdcddb42d99664f0db9ed466751c3d1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bcf868ad825f46a0f18df9029bcf8b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cdc0c204db44fd1a85f",
+    "url": "/static/js/2.aa05d5bf.chunk.js"
+  },
+  {
+    "revision": "5854e05569b571b52e88",
+    "url": "/static/js/main.d2328f92.chunk.js"
+  },
+  {
+    "revision": "ab07d74e1bdcb986af0d",
+    "url": "/static/js/runtime-main.f28ad287.js"
+  }
+]);
